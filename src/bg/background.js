c6fcc9ea62013b3ee139531e67ffb5607101c7e9
Matt Price <matt.price@utoronto.ca>
2016-12-15 23:39:45 -0500

    Unsuccessful changes
    trying to add some extra info to the get url, but appear to have broken
    the app.  oops.

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,6 +3,7 @@ var GOOGLE_FORMS_URL = 'https://docs.google.com/forms/d/1kuwxu2lXYSRpkwBj4o9kwjU
 var NAME_FIELD = '&entry.604638068=';
 var EVENTNAME_FIELD = '&entry.1800984457=';
 var URL_FIELD = '&entry.242612017=';
+var COMMENT_FIELD = '&entry.1562922032=';
 
 //On click action for Extension
 chrome.browserAction.onClicked.addListener( function( tab ) {
@@ -10,8 +11,9 @@ chrome.browserAction.onClicked.addListener( function( tab ) {
   var currentURL = tab.url.replace( 'https://', '' );
 
   //instantiate locaStorage Variables
-  default_name = localStorage["name"] || 'Enter your name ';
-  default_EventName = localStorage["eventName"] || 'Enter event name';
+    default_name = localStorage["name"] || 'Enter your name ';
+    default_EventName = localStorage["eventName"] || 'Enter event name';
+    default_email = localStorage["email"] || 'Enter your email or type a space to make this message disappear! ';
 
   var notificationToolUrl = 'http://digital2.library.unt.edu/nomination/eth2016/url/';
 
@@ -23,17 +25,30 @@ chrome.browserAction.onClicked.addListener( function( tab ) {
     localStorage["name"] = nameAnswer;
   };
 
+
   //check if localStorage has event anem if not prompt
   if(!localStorage["eventName"]){
     var eventAnswer = prompt('Please enter your event name', default_EventName);
     localStorage["eventName"] = eventAnswer;
   };
+
+    //check if localStorage has email if not prompt
+    if(!localStorage["email"]){
+        var emailAnswer = prompt('Please enter your email',default_email);
+        localStorage["email"] = emailAnswer;
+    };
+
+    // allow a comment on the url:
+    var commentAnswer = prompt('Please enter any further helpful information', "");
+
   // Do GET call to post to Google Form and open new tab
   $.get({
-    url: GOOGLE_FORMS_URL + NAME_FIELD + localStorage["name"] + EVENTNAME_FIELD + localStorage["eventName"]
-    + URL_FIELD + currentURL+'&submit=Submit',
-    success: function(res){
-      window.open(notificationToolUrl + currentURL);
+    url:GOOGLE_FORMS_URL + NAME_FIELD + localStorage["name"] + EVENTNAME_FIELD + localStorage["eventName"]
+          + URL_FIELD + currentURL + COMMENT_FIELD + commentAnswer +'&submit=Submit',
+      success: function(res){
+          console.log("successfully logged " + url + "to the event spreadsheet.");
+          alert("thank you!");
+     // window.open(notificationToolUrl + currentURL);
     },
     error: function(err){
       console.error(err);
