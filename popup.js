b0613e0f28ec297218de808151c9da6f21f45e2d
Ates Goral <ates.g@myplanet.com>
2017-04-05 02:36:53 -0400

    Simultaneously submit code to UNT Government Web & Data Archive

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 /* List of Agency Office Codes and their corresponding Department names
 */
-var AGENCY_IDS = {
+var agencyIds = {
   "1": "Environmental Protection Agency",
   "2": "Energy Department",
   "3": "National Oceanic and Atmospheric Administration",
@@ -132,172 +132,186 @@ function checkUrl() {
     });
 }
 
-function onPageDetailsReceived( pageDetails ) {
+function checkAgency() {
+  /* Disable the Department/Agency dropdown bar once the agency code has been entered.
+  * Changing the code will re-enter the correct department/agency name.
+  */
+  var agencyId = $('#agencyId').val();
+  var agencyName = agencyIds[agencyId];
+
+  if (agencyName) {
+    $("#agencyName").val(agencyName).prop('disabled', true);
+  } else {
+    $("#agencyName").prop('disabled', false);
+  }
+}
+
+function onPageDetailsReceived(pageDetails) {
   $('#title').val(pageDetails.title);
   $('#url').val(pageDetails.url);
   checkUrl();
+}
+
+function getCookieValue(url, name) {
+  return new Promise(function (resolve, reject) {
+    chrome.cookies.get({
+      url: url,
+      name: name
+    }, function (cookie) {
+      if (cookie) {
+        resolve(cookie.value);
+      } else {
+        reject();
+      }
+    });
+  });
+}
+
+function submitToGoogleForms(fields) {
+  console.log('Submitting to Google Forms');
+
+  var url = 'https://docs.google.com/forms/d/1udxf9C7XeO7rm-SoucjDIv0c8XzGb7VVutsCI8r4s-Y/formResponse';
+
+  var fieldConversion = {
+    title: 180917746,
+    url: 1767515686,
+    nominatorName: 1227505863,
+    nominatorEmail: 1975141568,
+    eventName: 1518336548,
+    agencyName: 1285343614,
+    agencyId: 536064408,
+    subAgencyId: 1706245913,
+    organizationId: 1076101938,
+    suborgId: 1768657731,
+    subprimerId: 615621344,
+    hasFtp: 365628902,
+    hasVisualization: 2057247667,
+    hasManyFiles: 1702958174,
+    hasDatabase: 15716096,
+    comments: 2034225983
+  };
+
+  var query = { ifq: '' };
+
+  for (var fieldName in fieldConversion) {
+    var fieldValue = fields[fieldName];
+    var queryParamName = 'entry.' + fieldConversion[fieldName];
+
+    query[queryParamName] = typeof fieldValue === 'boolean'
+      ? fieldValue ? 'Yes' : ''
+      : fieldValue;
+  }
+
+  return $.get(url, query)
+    .then(function () {
+      console.log('Submitted to Google Forms');
+    });
+}
+
+function submitToUntGwda(fields) {
+  console.log('Submitting to UNT GWDA');
+
+  var url = 'http://digital2.library.unt.edu/nomination/GWDA/add/';
+
+  var fieldConversion = {
+    url: 'url_value',
+    nominatorName: 'nominator_name',
+    nominatorEmail: 'nominator_email',
+    eventName: 'nominator_institution'
+  };
+
+  return $.get(url) // Load form to page to get CSRF cookie
+    .then(function () {
+      console.log('Loaded form page');
+
+      return getCookieValue(url, 'csrftoken');
+    })
+    .then(function (csrfToken) {
+      console.log('Got CSRF token');
+
+      var body = { csrfmiddlewaretoken: csrfToken };
+
+      for (var fieldName in fieldConversion) {
+        var fieldValue = fields[fieldName];
+        var bodyParamName = fieldConversion[fieldName];
+
+        body[bodyParamName] = fieldValue;
+      }
+
+      return $.post(url, body)
+        .then(function () {
+          console.log('Submitted to UNT GWDA');
+        });
+    });
 }
 
 // POST the data to the server using XMLHttpRequest
 function handleSubmit( e ) {
   e.preventDefault();
 
+  var fields = {
+    url: $('#url').val(),
+    title: $('#title').val(),
+    nominatorName: $('#nominatorName').val(),
+    nominatorEmail: $('#nominatorEmail').val(),
+    eventName: $('#eventName').val(),
+    agencyName: $('#agencyName').val(),
+    agencyId: $('#agencyId').val(),
+    subAgencyId: $('#subAgencyId').val(),
+    organizationId: $('#organizationId').val(),
+    suborgId: $('#suborgId').val(),
+    subprimerId: $('#subprimerId').val(),
+    hasFtp: !!$('#hasFtp:checked').val(),
+    hasVisualization: !!$('#hasVisualization:checked').val(),
+    hasManyFiles: !!$('#hasManyFiles:checked').val(),
+    hasDatabase: !!$('#hasDatabase:checked').val(),
+    comments: $('#comments').val()
+  };
+
+  localStorage.nominatorName = fields.nominatorName;
+  localStorage.nominatorEmail = fields.nominatorEmail;
+  localStorage.eventName = fields.eventName;
+  localStorage.agencyName = fields.agencyName;
+  localStorage.agencyId = fields.agencyId;
+  localStorage.subAgencyId = fields.subAgencyId;
+  localStorage.organizationId = fields.organizationId;
+  localStorage.suborgId = fields.suborgId;
+  localStorage.subprimerId = fields.subprimerId;
+
   showStatus('pending', 'Submitting');
 
   $('#submit').prop('disabled', true);
 
-  // Google Forms constants
-  var GOOGLE_FORMS_URL = 'https://docs.google.com/forms/d/1udxf9C7XeO7rm-SoucjDIv0c8XzGb7VVutsCI8r4s-Y/formResponse';
-  var NAME_FIELD = 'entry.1227505863';
-  var EMAIL_FIELD = 'entry.1975141568';
-  var EVENTNAME_FIELD = 'entry.1518336548';
-  var URL_FIELD = 'entry.1767515686';
-  var TITLE_FIELD = 'entry.180917746';
-
-  var NOTIFICATION_TOOL_URL = 'http://digital2.library.unt.edu/nomination/eth2016/url/';
-  var AGENCY_FIELD = 'entry.1285343614';
-  var AGENCY_ID = 'entry.536064408';
-  var SUBAGENCY_ID = 'entry.1706245913';
-  var ORGANIZATION_ID = 'entry.1076101938';
-  var SUBORG_ID = 'entry.1768657731';
-  var SUBPRIMER_ID = 'entry.615621344';
-  var FTP_ID = 'entry.365628902';
-  var VISUALIZATION_ID = 'entry.2057247667';
-  var DIFFICULTY_ID = 'entry.1702958174';
-  var DATABASE_ID = 'entry.15716096';
-  var COMMMENT_ID = 'entry.2034225983';
-  //var CRAWLABLE_ID = 'entry.2059306163';
-
-  var title = $( '#title' ).val();
-  var name = $( '#name' ).val();
-  var email = $( '#email' ).val();
-  // make constant for today's event
-  // var eventName = "Guerilla Archiving Toronto Dec. 17"
-  var eventName = $( '#eventName' ).val();
-  var currentURL = $( '#url' ).val();
-  var agency = $( '#agency' ).val();
-  var agencyID = $( '#agencyID' ).val();
-  var subAgencyID = $( '#subAgencyID' ).val();
-  var organizationID = $( '#organizationID' ).val();
-  var suborgID = $( '#suborgID' ).val();
-  var subprimerID = $( '#subprimerID' ).val();
-  var ftpID = !!$( '#ftpID:checked' ).val();
-  var visualizationID = !!$( '#visualizationID:checked').val();
-  var difficultyID = !!$( '#difficultyID:checked').val();
-  var databaseID = !!$( '#databaseID:checked').val();
-  var commentID = $ ( '#commentID').val();
-
-  if ( localStorage.name !== name ) {
-    localStorage.name = name;
-  }
-  if ( localStorage.email !== email ) {
-    localStorage.email = email;
-  }
-  if ( localStorage.eventName !== eventName ) {
-    localStorage.eventName = eventName;
-  }
-  if ( localStorage.agency !== agency ) {
-    localStorage.agency = agency;
-  }
-
-  if ( localStorage.agencyID !== agencyID ) {
-    localStorage.agencyID = agencyID;
-  }
-  if ( localStorage.subAgencyID !== subAgencyID ) {
-    localStorage.subAgencyID = subAgencyID;
-  }
-  if ( localStorage.organizationID !== organizationID ) {
-    localStorage.organizationID = organizationID;
-  }
-  if ( localStorage.suborgID !== suborgID ) {
-    localStorage.suborgID = suborgID;
-  }
-
-  if ( localStorage.subprimerID !== subprimerID ) {
-    localStorage.subprimerID = subprimerID;
-  }
-
-  var data = { ifq: '' };
-
-  data[NAME_FIELD] = localStorage.name;
-  data[EMAIL_FIELD] = localStorage.email;
-  data[TITLE_FIELD] = title;
-  data[EVENTNAME_FIELD] = localStorage.eventName;
-  data[URL_FIELD] = currentURL;
-  data[AGENCY_FIELD] = agency;
-  data[AGENCY_ID] = agencyID;
-  data[SUBAGENCY_ID] = subAgencyID;
-  data[ORGANIZATION_ID] = organizationID;
-  data[SUBORG_ID] = suborgID;
-  data[SUBPRIMER_ID] = subprimerID;
-  //data[CRAWLABLE_ID] = crawlableID;
-  data[FTP_ID] = ftpID ? 'Yes' : '';
-  data[VISUALIZATION_ID] = visualizationID ? 'Yes' : '';
-  data[DIFFICULTY_ID] = difficultyID ? 'Yes' : '';
-  data[DATABASE_ID] = databaseID ? 'Yes' : '';
-  data[COMMMENT_ID] = commentID;
-
-  $.get(GOOGLE_FORMS_URL, data)
+  $.when(submitToGoogleForms(fields), submitToUntGwda(fields))
     .then(function () {
       $('#submit').prop('disabled', false);
 
       showStatus('success', 'URL submitted. Thanks!');
       hideStatus(3000);
-      // uncomment this line to also add the URL through the official notificaiton tool.
-      // window.open(NOTIFICATION_TOOL_URL + currentURL);
     })
     .catch(function (error) {
       $('#submit').prop('disabled', false);
-      showStatus('failure', 'Could not submit URL (' + (error.statusText || 'Generic error') + ')');
-    })
+      showStatus('failure', 'Could not submit URL');
+    });
 }
 
 /* When the popup loads: Autopopulate the name, event name and email if it has been submitted before,
 * i.e. if localStorage has these fields already saved.
 */
 $(function () {
-  if ( localStorage.name && localStorage.name !== "null" ) {
-    $( '#name' ).val( localStorage.name );
-  }
-  if ( localStorage.email && localStorage.email !== "null" ) {
-    $( '#email' ).val( localStorage.email );
-  }
-  if ( localStorage.eventName && localStorage.eventName !== "null" ) {
-    $( '#eventName' ).val( localStorage.eventName );
-  }
-  if ( localStorage.agency && localStorage.agency !== "null" ) {
-    $( '#agency' ).val( localStorage.agency );
-  }
+  $('#nominatorName').val(localStorage.nominatorName);
+  $('#nominatorEmail').val(localStorage.nominatorEmail);
+  $('#eventName').val(localStorage.eventName);
+  $('#agencyName').val(localStorage.agencyName);
+  $('#agencyId').val(localStorage.agencyId);
+  $('#subAgencyId').val(localStorage.subAgencyId);
+  $('#organizationId').val(localStorage.organizationId);
+  $('#suborgId').val(localStorage.suborgId);
+  $('#subprimerId').val(localStorage.subprimerId);
 
-  if ( localStorage.agencyID && localStorage.agencyID !== "null" ) {
-    $( '#agencyID' ).val( localStorage.agencyID );
-  }
-  if ( localStorage.subAgencyID && localStorage.subAgencyID !== "null" ) {
-    $( '#subAgencyID' ).val( localStorage.subAgencyID );
-  }
-  if ( localStorage.organizationID && localStorage.organizationID !== "null" ) {
-    $( '#organizationID' ).val( localStorage.organizationID );
-  }
-  if ( localStorage.suborgID && localStorage.suborgID !== "null" ) {
-    $( '#suborgID' ).val( localStorage.suborgID );
-  }
-  if ( localStorage.subprimerID && localStorage.subprimerID !== "null" ) {
-    $( '#subprimerID' ).val( localStorage.subprimerID );
-  }
+  checkAgency();
 
-  /* Disable the Department/Agency dropdown bar once the agency code has been entered.
-  * Changing the code will re-enter the correct department/agency name.
-  */
-  $( '#agencyID' ).change( function( event ) {
-    var enteredCode = $( event.currentTarget ).val();
-    var agencyName = AGENCY_IDS[ enteredCode ];
-
-    if (agencyName) {
-      $( "#agency" ).val( agencyName ).prop('disabled', true);
-    } else {
-      $( "#agency" ).prop('disabled', false);
-    }
-  } );
+  $('#agencyId').change(checkAgency);
 
   $('#url').on('keyup paste change', debounce(checkUrl, 500));
 
@@ -310,10 +324,10 @@ $(function () {
   $('#form').submit(handleSubmit);
 
   // Get the event page
-  chrome.runtime.getBackgroundPage( function( eventPage ) {
+  chrome.runtime.getBackgroundPage(function (eventPage) {
     // Call the getPageInfo function in the event page, passing in
     // our onPageDetailsReceived function as the callback. This injects
     // content.js into the current tab's HTML
-    eventPage.getPageDetails( onPageDetailsReceived );
-  } );
+    eventPage.getPageDetails(onPageDetailsReceived);
+  });
 });
